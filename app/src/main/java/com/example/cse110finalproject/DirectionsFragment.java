23cc7f290f43b1a9af41ab04fa5d6fb1a895d4b9
jinuk6789 <jinuk6789@gmail.com>
2022-04-29 15:06:46 -0700

    Added menu bar

---
+++ app/src/main/java/com/example/cse110finalproject/DirectionsFragment.java
@@ -0,0 +1,20 @@
+package com.example.cse110finalproject;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class DirectionsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_directions, container, false);
+    }
+}
--- app/src/main/java/com/example/cse110finalproject/MainActivity.java
+++ app/src/main/java/com/example/cse110finalproject/MainActivity.java
@@ -1,18 +1,48 @@
 package com.example.cse110finalproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    SearchFragment searchFragment = new SearchFragment();
+    PlanFragment planFragment = new PlanFragment();
+    DirectionsFragment directionsFragment = new DirectionsFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this,SearchActivity.class);
-        startActivity(intent);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,searchFragment).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.search_menu:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,searchFragment).commit();
+                        return true;
+                    case R.id.plan_menu:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,planFragment).commit();
+                        return true;
+                    case R.id.directions_menu:_menu:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container,directionsFragment).commit();
+                        return true;
+                }
+
+                return false;
+            }
+        });
     }
 }
