d49f0c9c077ea695e4b96e1d7348d8d9925ecd4f
TorinF <tfaes@sweetwaterschools.net>
2022-05-04 16:52:12 -0700

    Starting on instrumented test for fragments

--- app/src/androidTest/java/com/example/cse110finalproject/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/cse110finalproject/ExampleInstrumentedTest.java
@@ -2,14 +2,20 @@ package com.example.cse110finalproject;
 
 import android.content.Context;
 
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
-
+import androidx.test.espresso.Espresso;
 /**
  * Instrumented test, which will execute on an Android device.
  *
@@ -17,10 +23,18 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    @Rule
+    public ActivityScenarioRule rule = new ActivityScenarioRule<>(MainActivity.class);
+
     @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.cse110finalproject", appContext.getPackageName());
+    public void testingFragment() {
+        //Given
+        ActivityScenario<MainActivity> activity = rule.getScenario();
+
+        //When
+
+        assert(1==1);
+        //Then
+
     }
 }
