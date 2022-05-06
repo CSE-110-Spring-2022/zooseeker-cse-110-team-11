8c1a4314fa050ae493e08936cd29fcc6ddac6a16
TorinF <tfaes@sweetwaterschools.net>
2022-05-05 20:14:31 -0700

    Filtered out planned exhibits for plan tab

--- app/src/main/java/com/example/cse110finalproject/PlanViewModel.java
+++ app/src/main/java/com/example/cse110finalproject/PlanViewModel.java
@@ -36,6 +36,6 @@ public class PlanViewModel extends AndroidViewModel {
 
     //TODO Implement the database
     private void loadPlans() {
-        searchItems = searchPlacesDao.getSearchItemsLive();
+        searchItems = searchPlacesDao.getPlannedItemsLive();
     }
 }
