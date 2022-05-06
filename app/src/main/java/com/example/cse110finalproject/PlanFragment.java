343b92309e1125d1a622087767b81010a679b6b8
jinuk6789 <jinuk6789@gmail.com>
2022-05-05 19:57:28 -0700

    Plan Tab Working Now

--- app/src/main/java/com/example/cse110finalproject/PlanFragment.java
+++ app/src/main/java/com/example/cse110finalproject/PlanFragment.java
@@ -41,10 +41,6 @@ public class PlanFragment extends Fragment {
         PlanListAdapter adapter = new PlanListAdapter();
         if(getViewLifecycleOwner()==null)
             Log.d("confustion", "what the fuck");
-        ZooData.VertexInfo[] example_array = {MainActivity.exhibitsList.get(1)};
-        List<ZooData.VertexInfo> example_arrlst = new ArrayList<ZooData.VertexInfo>(Arrays.asList(example_array));
-        List<Places> example_places = Places.convertVertexListToPlaces(example_arrlst);
-        adapter.setSearchItem(example_places);
         viewModel.getSearchItems().observe(getViewLifecycleOwner(), adapter::setSearchItem);
 
         recyclerView = rootView.findViewById(R.id.plan_items);
