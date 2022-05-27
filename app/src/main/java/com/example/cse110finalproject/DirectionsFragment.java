caaaaaa6ac19306ddfb7f56f98d16c93c06a6c86
TorinF <tfaes@sweetwaterschools.net>
2022-05-27 15:06:43 -0700

    Imported the new assets

--- app/src/main/java/com/example/cse110finalproject/DirectionsFragment.java
+++ app/src/main/java/com/example/cse110finalproject/DirectionsFragment.java
@@ -10,6 +10,7 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
